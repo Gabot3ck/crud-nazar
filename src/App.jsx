a3f3648cc71d907e42980e7863e3e97095309cee
Gabot3ck <jesus.castilloc@outlook.com>
2024-05-08 21:28:56 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-red-800 font-bold bg-primary-500" >HELLO WORLD</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+            400: '#4327D0',
+            500: '#2b0ebe',
+            600: '#190773',
+        },
+        'light': {
+          500: '#f2f2f2'
+        },
+        'alert': {
+          500: '#ef4444',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
